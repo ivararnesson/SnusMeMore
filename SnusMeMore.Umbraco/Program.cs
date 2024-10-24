08b8bdf498486e0a02ca194fd0c26ab33eaf434f
Ivar Arnesson <ivar.arnesson@gmail.com>
2024-10-24 13:41:14 +0200

    Added React as frontend

--- SnusMeMore.Umbraco/Program.cs
+++ SnusMeMore.Umbraco/Program.cs
@@ -1,4 +1,10 @@
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
@@ -6,7 +12,28 @@ builder.CreateUmbracoBuilder()
     .AddComposers()
     .Build();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
 WebApplication app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAll");
+app.UseHttpsRedirection();
 
 await app.BootUmbracoAsync();
 
@@ -22,5 +49,28 @@ app.UseUmbraco()
         u.UseBackOfficeEndpoints();
         u.UseWebsiteEndpoints();
     });
+
+app.MapGet("api/content/{guid:guid}", (Guid guid, IUmbracoContextAccessor umbracoContextAccessor) =>
+{
+    var umbracoContext = umbracoContextAccessor.GetRequiredUmbracoContext();
+    var content = umbracoContext.Content.GetById(guid);
+
+    if (content == null)
+    {
+        return Results.NotFound();
+    }
+
+    var result = new
+    {
+        Title = content.Value<string>("title"),
+        Home = content.Value<string>("home"),
+        OptionOne = content.Value<string>("optionOne"),
+        OptionTwo = content.Value<string>("optionTwo"),
+        OptionThree = content.Value<string>("optionThree"),
+        ShoppingCart = content.Value<string>("shoppingCart"),
+    };
+
+    return Results.Ok(result);
+});
 
 await app.RunAsync();
--- SnusMeMore.Web/src/App.jsx
+++ SnusMeMore.Web/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import "./assets/CSS/master.css"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [documentModel, setDocumentModel] = useState("")
+
+  const getDocumentValues = async () => {
+    fetch("https://localhost:44311/api/content/cf233671-9951-42ce-894b-b8a7d8a9aaeb")
+    .then(respons => respons.json())
+    .then(result => {
+      setDocumentModel(result) 
+      console.log(result)
+    })
+  }
+
+  useEffect(() => {
+    getDocumentValues()
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <nav className="master--nav-container">
+      <h1 className="master--nav-title">{documentModel.title}</h1>
+      <div className="master--li-container">
+        <ul className="master--nav-ul">
+          <li className="master--nav-li">
+            <a className="master--nav-li-btn" href="/">{documentModel.home}</a>
+          </li>
+          <li className="master--nav-li">
+            <a className="master--nav-li-btn" href="/">{documentModel.optionOne}</a>
+          </li>
+          <li className="master--nav-li">
+            <a className="master--nav-li-btn" href="/">{documentModel.optionTwo}</a>
+          </li>
+          <li className="master--nav-li">
+            <a className="master--nav-li-btn" href="/">{documentModel.optionThree}</a>
+          </li>
+        </ul>
+        <a className="master--nav-li-btn">{documentModel.shoppingCart}</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </nav>
   )
 }
 
