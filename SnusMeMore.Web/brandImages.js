82bd857ef6226e7a141ae800d3b3a9d3f43b7bf9
Ivar Arnesson <ivar.arnesson@gmail.com>
2024-10-29 15:09:58 +0100

    made a new branch to merge to main from snusshowlist

---
+++ SnusMeMore.Web/brandImages.js
@@ -0,0 +1,27 @@
+import Knox from "./src/assets/CSS/images/knox.png"
+import Kaliber from "./src/assets/CSS/images/kaliber-logo.png"
+import Lundgrens from "./src/assets/CSS/images/Lundgrens-logo.png"
+import One from "./src/assets/CSS/images/ONE-logo.png"
+import Velo from "./src/assets/CSS/images/velo-snus-logo.png"
+import Granit from "./src/assets/CSS/images/granit-logo.png"
+import General from "./src/assets/CSS/images/general-logo.png"
+import Ld from "./src/assets/CSS/images/ld-logo.png"
+import Loop from "./src/assets/CSS/images/loop-logo.png"
+import Skruf from "./src/assets/CSS/images/Skruf-logo.png"
+import Xr from "./src/assets/CSS/images/xr-logo.png"
+import Zyn from "./src/assets/CSS/images/ZYN-Logo.png"
+
+export const brandImages = [
+    {imgSrc: Knox, altTxt:"Knox"},
+    {imgSrc: Velo, altTxt:"Velo"},
+    {imgSrc: Lundgrens, altTxt:"Lundgrens"},
+    {imgSrc: One, altTxt:"One"},
+    {imgSrc: Kaliber, altTxt:"Kaliber"},
+    {imgSrc: Granit, altTxt:"Granit"},
+    {imgSrc: Ld, altTxt:"Ld"},
+    {imgSrc: Skruf, altTxt:"Skruf"},
+    {imgSrc: Xr, altTxt:"Xr"},
+    {imgSrc: Zyn, altTxt:"Zyn"},
+    {imgSrc: General, altTxt:"General"},
+    {imgSrc: Loop, altTxt:"Loop"},
+]
