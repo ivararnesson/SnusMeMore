3151cbffe882637d4f0f1e2a7fa225501f913d91
Ivar Arnesson <ivar.arnesson@gmail.com>
2024-11-05 10:04:19 +0100

    some styling for the footer

--- SnusMeMore.Web/src/components/Footer.jsx
+++ SnusMeMore.Web/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="footer--container">
             <div className="footer--list-container">
                 <ul className="footer--ul">
-                    <li className="footer--li-1">SnusMeMore</li> 
+                    <li className="footer--li-1">SNUSMEMORE</li> 
                     <li className="footer--list-item">
                         <Link to="/" className="footer--list-link">SnusMeMore Koncept</Link>
                     </li>
@@ -22,7 +22,7 @@ const Footer = () => {
 
             <div className="footer--list-container">
                 <ul className="footer--ul">
-                    <li className="footer--li-1">Support</li>
+                    <li className="footer--li-1">SUPPORT</li>
                     <li className="footer--list-item">
                         <Link to="/" className="footer--list-link">FAQ</Link>
                     </li>
@@ -38,14 +38,14 @@ const Footer = () => {
                 </ul>
             </div>
 
-            <div className="footer--list-container">
+            {/* <div className="footer--list-container">
                 <ul className="footer--ul">
-                    <li className="footer--li-1">Betala med</li>
+                    <li className="footer--li-1">BETALA MED</li>
                     <li className="footer--list-item">
-                        <Link to="/" className="footer--list-link">SnusMeMore Koncept</Link>
+                        <Link to="/" className="footer--list-link-payment">Mastercard/Visa</Link>
                     </li>
                 </ul>
-            </div>
+            </div> */}
         </div>
     )
 }
